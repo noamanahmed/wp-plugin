bb7a6a9324c8c1f24d04efe5f2075039ddd2b50c
Your Name <you@example.com>
2022-09-09 22:45:46 +0500

    Added Shortcodes Folder

---
+++ noaman.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * noaman
+ *
+ * @package       NOAMAN
+ * @author        Noaman Ahmed
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   noaman
+ * Plugin URI:    https://noamanahmed.com
+ * Description:   Test Plugin
+ * Version:       1.0.0
+ * Author:        Noaman Ahmed
+ * Author URI:    https://noamanahmed.com
+ * Text Domain:   noaman
+ * Domain Path:   /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Include your custom code here.
