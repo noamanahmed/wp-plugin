ad2bb0a27f006866acaefbe62b895fa2efc6e4e3
Your Name <you@example.com>
2022-09-10 01:41:48 +0500

    Added PHPUnit Testing to the plugin

---
+++ tests/AssetsTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Noaman
+ */
+
+/**
+ * Sample test case.
+ */
+class AssetsTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_scripts_enqueued() {
+		do_action('wp_enqueue_scripts');
+
+		$this->assertTrue(wp_script_is('magnific-popup.min.js'));
+		$this->assertTrue(wp_script_is('magnific-popup-init.js'));
+		$this->assertTrue(wp_style_is('magnific-popup.min.css'));
+		
+	}
+
+}
---
+++ tests/ShortCodeTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Noaman
+ */
+
+/**
+ * Sample test case.
+ */
+class ShortCodeTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_images_search_shortcode_exists() {
+		
+		$this->assertTrue(shortcode_exists('images_search'));
+	}
+
+	public function test_images_search_shortcode_doesnot_throw_error() {
+		$content = do_shortcode('[images_search]');		
+		$this->assertTrue(TRUE);
+	}
+
+}
